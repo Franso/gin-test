78caa419f21857b580c1edf4e99c45e60e683369
Franso <francismuasya@gmail.com>
2021-05-08 13:06:18 +0300

    added json/xml support

--- handlers.article.go
+++ handlers.article.go
@@ -11,17 +11,22 @@ func showIndexPage(c *gin.Context) {
 	articles := getAllArticles()
 
 	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HML status to 200
-		http.StatusOK,
-		// use the index.html template
-		"index.html",
-		// Pass the data that the page uses
-		gin.H{
-			"title":   "Home Page",
-			"payload": articles,
-		},
-	)
+	// c.HTML(
+	// 	// Set the HML status to 200
+	// 	http.StatusOK,
+	// 	// use the index.html template
+	// 	"index.html",
+	// 	// Pass the data that the page uses
+	// 	gin.H{
+	// 		"title":   "Home Page",
+	// 		"payload": articles,
+	// 	},
+	// )
+	// Call the render function with the name of the template to render
+	render(c, gin.H{
+		"title":   "Home Page",
+		"payload": articles}, "index.html")
+
 }
 
 func getArticle(c *gin.Context) {
@@ -30,17 +35,21 @@ func getArticle(c *gin.Context) {
 		// Check if the article exist
 		if article, err := getArticleByID(articleID); err == nil {
 			// call the HTML method of the Context to render a template
-			c.HTML(
-				// set the HTTP status to 200 (OK)
-				http.StatusOK,
-				// Use the index.html page
-				"article.html",
-				// Pass the data that the page uses
-				gin.H{
-					"title":   article.Title,
-					"payload": article,
-				},
-			)
+			render(c, gin.H{
+				"title":   article.Title,
+				"payload": article,
+			}, "article.html")
+			// c.HTML(
+			// 	// set the HTTP status to 200 (OK)
+			// 	http.StatusOK,
+			// 	// Use the index.html page
+			// 	"article.html",
+			// 	// Pass the data that the page uses
+			// 	gin.H{
+			// 		"title":   article.Title,
+			// 		"payload": article,
+			// 	},
+			// )
 		} else {
 			// If article is not found, abort with an error
 			c.AbortWithError(http.StatusNotFound, err)
